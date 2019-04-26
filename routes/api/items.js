07baee13492a5b7ca3665888fa59ed4b39ebd6ea
Langdon <lfroker@gmail.com>
2019-04-26 11:40:30 -0700

    added CRUD functionality

--- routes/api/items.js
+++ routes/api/items.js
@@ -14,4 +14,26 @@ router.get('/', (req, res)=>{
     .catch(err => console.log(err))
 });
 
+// @route   POST api/items
+// @desc    Create an item
+// @access  Public
+router.post('/', (req, res)=>{
+    const newItem = new Item({
+        name:req.body.name
+    });
+
+    newItem.save().then(item => res.json(item));
+});
+
+// @route   Delete api/items/:id
+// @desc    Delete item
+// @access  Public
+router.delete('/:id', (req, res)=>{
+    Item.findById(req.params.id)
+        .then(item => item.remove().then(()=>res.json({success:true})))
+        .catch(err => res.status(404).json({success:false}))
+    
+});
+
+
 module.exports = router
